ba226ef8c4469d745685072018aea5a9a56364d6
Hiromitsu Yamaguchi <yamagame@mac.com>
2025-05-05 17:36:43 +0900

    first commit

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+OPTION=$1
+
+if [ "$OPTION" == "REBUILD" ]; then
+    docker build -t pyxapp .
+fi
+docker run -it --rm -v $PWD/:/app -w /app/src -p 8000:8000 pyxapp pyxel package . main.py
+mv ./src/src.pyxapp ./out/copilot-adventure.pyxapp
